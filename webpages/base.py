ce86eee2b16e9d458b8839f9027498f409c28c10
Thepian <henrik@thepia.com>
2011-09-18 13:30:47 -0400

    browser switched

--- webpages/base.py
+++ webpages/base.py
@@ -1,3 +1,5 @@
+import re
+
 class ObjectLike(object):
 
     def __init__(self,d):
@@ -14,3 +16,28 @@ class ObjectLike(object):
         return self.data[key]
         
    
+def get_browser_type(userAgent):
+    """
+    http://developer.apple.com/library/safari/#documentation/AppleApplications/Reference/SafariWebContent/OptimizingforSafarioniPhone/OptimizingforSafarioniPhone.html#//apple_ref/doc/uid/TP40006517-SW3
+    http://wurfl.sourceforge.net/python/index.php
+    http://www.zytrax.com/tech/web/mobile_ids.html
+    
+    http://webservices.usc.edu/blog/development/mobile_detection/
+    
+    Samples:
+    Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.10
+    Mozilla/5.0 (iPhone; U; CPU iOS 2_0 like Mac OS X; en-us) AppleWebKit/525.18.1 (KHTML, like Gecko) Version/3.1.1 Mobile/XXXXX Safari/525.20
+    
+    """
+    MOBILE = re.compile(r" Mobile[/ ]")
+    IPAD = re.compile(r"\(iPad;")
+
+    if MOBILE.search(userAgent):
+        if IPAD.search(userAgent):
+            return "tablet"
+        if re.search(r"A43 ",userAgent) or re.search(r"X2 ",userAgent):
+            return "tablet"
+        return "pocket"
+
+    return "desktop"
+
--- webpages/cached.py
+++ webpages/cached.py
@@ -1,33 +1,114 @@
 from __future__ import with_statement
-from os.path import join, exists
+
+import os
+from os.path import join, exists, abspath, splitext
 from fs import listdir, filters
-import hashlib
-try:
-    import json
-except:
-    import simplejson as json
-import time
+import hashlib, json, datetime, time, site
+import mimetypes
 
 import tornado.web
 import redis
+
+from base import *
 
 UNIT_SEP = "\x1F"
 REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
 
 # Redis connection
-REDIS = redis.Redis(REDIS_HOST, REDIS_PORT, db=0)
+REDIS = redis.Redis(REDIS_HOST, REDIS_PORT, db=1)
 
 ONE_YEAR_IN_SECONDS = 365 * 24 * 3600
 IN_A_YEAR_STAMP = time.time() + ONE_YEAR_IN_SECONDS
 
+BROWSER_SPECIFIC_HEADER = "header:%s%s"
+BROWSER_SPECIFIC_CONTENT = "content:%s%s"
 
 class CachedHandler(tornado.web.RequestHandler):
     
-    def get(self,path):
-    	if path in REDIS:
-    		self.write(REDIS[path])
+    def head(self,path,browser_type=None):
+    	return self.get(path,browser_type,include_body=False)
+
+    def get(self,path,browser_type=None,include_body=True):
+        if not browser_type:
+            browser_type = get_browser_type( self.request.headers['User-Agent'] )
+
+        contentkey = BROWSER_SPECIFIC_CONTENT % (browser_type , path) 
+        headerkey = BROWSER_SPECIFIC_HEADER % (browser_type , path) 
+    	if contentkey in REDIS:
+    		#TODO etag and headers
+    		#TODO url type, inject state script
+    		if not include_body:
+	    		self.flush()
+    			return
+    		self.write(REDIS[contentkey])
     		self.flush()
     	else:
     		raise tornado.web.HTTPError(404, "We couldn't find requested information")
-        
+     
+#TODO review tornado StaticFileHandler
+
+def html_file(path,modified,content):
+	header = {
+		"Content-Type": "text/html",
+	}
+	return header,content
+	   
+def markdown_file(path,modified,content):
+	header = {
+		"Content-Type": "text/html",
+	}
+	#TODO convert markdown to html
+	return header,content
+	   
+def default_file(path,modified,content):
+	header = {
+		"Content-Type": "text/plain",
+	}
+	mime_type,encoding = mimetypes.guess_type(path)
+	if mime_type:
+		header["Content-Type"] = mime_type
+	return header,content
+
+FILE_EXTENSIONS = {
+	"html" : html_file,
+	"md" : markdown_file,
+	"mdown" : markdown_file,
+	"markdown" : markdown_file,
+}
+
+class FileExpander(object):
+
+	def __init__(self,base,relpath)
+		self.base = base
+		self.path = relpath
+
+		path = abspath(join(site.SITE_DIR,relpath))
+		stat_result = os.stat(path)
+		modified = datatime.datetime.fromtimestamp(stat_result[stat.ST_MTIME])
+
+		content = None
+		with open(path, "rb") as f:
+			content = f.read()
+
+		ext = splitext(path)[1]
+		if ext in FILE_EXTENSIONS:
+			self.header, self.content = FILE_EXTENSIONS[ext](path,modified,content)
+		else:
+			self.header, self.content = default_file(path,modified,content)
+
+
+	def cache(self,browser_type)
+        contentkey = BROWSER_SPECIFIC_CONTENT % (browser_type , self.path) 
+        REDIS[contentkey] = self.content
+        headerkey = BROWSER_SPECIFIC_HEADER % (browser_type , self.path) 
+        REDIS[headerkey] = self.header
+
+def populate_cache():
+	for relpath in fs.listdir(site.SITE_DIR,filters=filters.no_directories):
+		if not relpath[0] == "_":
+			expander = FileExpander(site.SITE_DIR,relpath)
+			expander.cache("pocket")
+			expander.cache("tablet")
+			expander.cache("desktop")
+
